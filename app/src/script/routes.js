10ad156ac9c96eb288718919859977a17e13e782
Virtuakeo <143969868+Virtuakeo@users.noreply.github.com>
2023-12-07 22:50:50 -0300

    Testing News Routes modes

---
+++ app/src/script/routes.js
@@ -0,0 +1,22 @@
+const route = (event) => {
+    event = event || window.event;
+    window.history.pushState({}, "", event.target.href);
+    handlePath();
+};
+
+const routes = {
+    "/": "index.html",
+    "/news": "./app/src/view/virtuakeo_news_initial/virtuakeo_news_initial.html",
+    "/contato": "https://wa.me/5531995601785?text=Gostaria%20de%20contratar%20seus%20serviços",
+};
+
+const handlePath = async () => {
+    const path = window.location.pathname;
+    const routes = route[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("container_home").innerHTML = html;
+};
+
+window.onpopstate = handlePath;
+window = route;
+handlePath();
